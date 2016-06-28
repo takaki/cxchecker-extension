108e8e1bccd1c5308a981206c2ce40506d6d4480
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-06-28 17:59:51 +0900

    jquery free

--- cxchecker.js
+++ cxchecker.js
@@ -1,21 +1,30 @@
-// Copyright (c) 2013 TANIGUCHI Takaki 
-// License: GPL version 3 or later  
+// Copyright (c) 2013 TANIGUCHI Takaki
+// License: GPL version 3 or later
 
-$(document).ready(function(){
+var onReady = function(){
     chrome.extension.onRequest.addListener(
-	function(request, sender, sendResponse) {
-	    if (request.type === 'css') {  
-		var result = $(request.query);
+        function(request, sender, sendResponse) {
+            if (request.type === 'css') {
+                var result = document.querySelectorAll(request.query)
             } else {
-                var result = $.xpath(request.query);
-	    }
-	    with(result){
-		css('background','#c88');
-		css('border','solid 2px red');
-	    }
-	    sendResponse({length: result.length});
-	}
+                var nodes = document.evaluate(request.query, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+                var result = []
+                for(var i=0;i<nodes.snapshotLength;i++){
+                    result.push(nodes.snapshotItem(i));
+                }
+            }
+            for(var i=0;i<result.length;i++){
+                var node = result[i];
+                node.style.background = '#c88';
+                node.style.border = 'solid 2px red';
+            }
+            sendResponse({length: result.length});
+        }
     );
-});
+}
 
-
+if (document.readyState !== 'loading') {
+    onReady();
+} else {
+    document.addEventListener('DOMContentLoaded', onReady);
+}
