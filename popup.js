cbc7fdd69d24cb0707442ebcc728ead5bf36cdcf
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-06-28 18:00:29 +0900

    Atom beautify

--- popup.js
+++ popup.js
@@ -1,65 +1,66 @@
-// Copyright (c) 2013 TANIGUCHI Takaki 
-// License: GPL version 3 or later  
+// Copyright (c) 2013 TANIGUCHI Takaki
+// License: GPL version 3 or later
 
-$(document).ready(function(){
+$(document).ready(function() {
     clear_css();
 
     var ls = localStorage['selector'];
     var type = localStorage['type'];
 
     if (type == 'xpath') {
-	$('input[name="type"]').val(['xpath']);
+        $('input[name="type"]').val(['xpath']);
     } else {
-	$('input[name="type"]').val(['css']);
+        $('input[name="type"]').val(['css']);
 
     }
 
-    $('input[name="type"]:radio').change( function() {  
-	localStorage['type'] = $(this).val();
-    });  
+    $('input[name="type"]:radio').change(function() {
+        localStorage['type'] = $(this).val();
+    });
 
 
     $('#selector').val(ls);
 
     $('#selector').bind(
-	'click blur keydown keyup keypress change',
-        function(){
+        'click blur keydown keyup keypress change',
+        function() {
             var selector = $(this).val();
-	    clear_css();
-	    localStorage['selector'] = selector;
+            clear_css();
+            localStorage['selector'] = selector;
 
-	    if ($('input[name="type"]:checked').val() == 'css'){
-		var type = 'css';
+            if ($('input[name="type"]:checked').val() == 'css') {
+                var type = 'css';
             } else {
-		var type = 'xpath';
-            }     
+                var type = 'xpath';
+            }
 
-	    chrome.tabs.getSelected(null, function(tab) {
-		chrome.tabs.sendRequest(
-		    tab.id, {
-			type: type,
-			query: selector
-		    },
-		    function(response) {
-			$('#match').text(response.length);
-			console.log(response.farewell);
-		    });
-	    });
-	});
+            chrome.tabs.getSelected(null, function(tab) {
+                chrome.tabs.sendRequest(
+                    tab.id, {
+                        type: type,
+                        query: selector
+                    },
+                    function(response) {
+                        $('#match').text(response.length);
+                        console.log(response.farewell);
+                    });
+            });
+        });
 
     $('#selector').click();
 });
 
-/*
-    $(window).unload(function(){
-	clear_css();
-    });
-*/
 
-function clear_css(){
+function clear_css() {
     chrome.tabs.executeScript(
-	null, {code:
-	       "$('*').css('background','');" +
-	       "$('*').css('border','')" 
-	      });
-};
+        null, {
+            code: "$('*').css('background','');" +
+                "$('*').css('border','')"
+        }
+    );
+    chrome.tabs.insertCSS(
+        null, {
+            code: "* {background: '', border: ''}"
+        }
+    );
+}
