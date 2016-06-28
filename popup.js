f4b19efa83f38eeffdbe75b43d7cc05b4894f4d5
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-06-28 19:24:42 +0900

    react

--- popup.js
+++ popup.js
@@ -1,22 +1,25 @@
 // Copyright (c) 2013 TANIGUCHI Takaki
 // License: GPL version 3 or later
 
+import  React from 'react';
+import  ReactDOM from 'react-dom';
+
 var onReady = function() {
     clear_css();
 
-    var ls = localStorage['selector'];
-    var type = localStorage['type'];
+    // var ls = localStorage['selector'];
+    // var type = localStorage['type'];
 
-    if (type == 'xpath') {
-        $('input[name="type"]').val(['xpath']);
-    } else {
-        $('input[name="type"]').val(['css']);
+    // if (type == 'xpath') {
+    //     $('input[name="type"]').val(['xpath']);
+    // } else {
+    //     $('input[name="type"]').val(['css']);
+    //
+    // }
 
-    }
-
-    $('input[name="type"]:radio').change(function() {
-        localStorage['type'] = $(this).val();
-    });
+    // $('input[name="type"]:radio').change(function() {
+    //     localStorage['type'] = $(this).val();
+    // });
 
 
     $('#selector').val(ls);
@@ -26,7 +29,7 @@ var onReady = function() {
         function() {
             var selector = $(this).val();
             clear_css();
-            localStorage['selector'] = selector;
+            // localStorage['selector'] = selector;
 
             if ($('input[name="type"]:checked').val() == 'css') {
                 var type = 'css';
@@ -64,3 +67,46 @@ if (document.readyState !== 'loading') {
 } else {
     document.addEventListener('DOMContentLoaded', onReady);
 }
+
+var App = React.createClass({
+    getInitialState: function() {
+        var selector = localStorage['selector'];
+        var type = localStorage['type'];
+        return {
+            type: type,
+            selector: selector,
+            match: 0,
+        }
+    },
+    _onRadioChange: function(e) {
+        clear_css();
+        var val = e.currentTarget.value;
+        localStorage['type'] = val;
+        this.setState({type: val})
+    },
+    _onTextChange: function(e) {
+        clear_css();
+        var val = e.currentTarget.value;
+        localStorage['selector'] = val;
+        this.setState({selector: val})
+    },
+    render: function() {
+        return (
+            <div>
+                <div>React CSS and XPath checker</div>
+                <input type="radio" name="rtype" value="css" onChange={this._onRadioChange} checked={this.state.type === 'css'} />
+                <label>CSS</label>
+                <input type="radio" name="rtype" value="xpath" onChange={this._onRadioChange} checked={this.state.type === 'xpath'} />
+                <label>XPath </label> /
+                <span>{this.state.match}</span> match(es)
+                <input type="text" size="40" value={this.state.selector} onChange={this._onTextChange}  />
+            </div>
+
+        )
+    }
+})
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+)
