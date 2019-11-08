0ea157afab0f4f0475b1c233e8351b9ba32a2e33
TANIGUCHI Takaki <takaki@asis.media-as.org>
2019-11-08 11:31:44 +0900

    error handling

--- src/cxchecker.ts
+++ src/cxchecker.ts
@@ -1,25 +1,28 @@
-// Copyright (c) 2013 TANIGUCHI Takaki
+// Copyright (c) 2013-2019 TANIGUCHI Takaki
 // License: GPL version 3 or later
 
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     cleanCss();
-    if (request.query === "") {
+    try {
+      if (request.type === 'css') {
+        var result = Array.from(document.querySelectorAll(request.query));
+      } else {
+        const nodes = document.evaluate(request.query, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+        var result = [];
+        for (var i = 0; i < nodes.snapshotLength; i++) {
+          result.push(nodes.snapshotItem(i));
+        }
+      }
+    } catch (e) {
       sendResponse({ length: 0 });
       return;
     }
-    if (request.type === 'css') {
-      var result = Array.from(document.querySelectorAll(request.query));
-    } else {
-      const nodes = document.evaluate(request.query, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-      var result = [];
-      for (var i = 0; i < nodes.snapshotLength; i++) {
-        result.push(nodes.snapshotItem(i));
-      }
-    }
     result.forEach((node) => {
-      node.style.background = '#c88';
-      node.style.border = 'solid 2px red';
+      if (node.style) {
+        node.style.background = '#c88';
+        node.style.border = 'solid 2px red';
+      }
     });
     sendResponse({ length: result.length });
   }
